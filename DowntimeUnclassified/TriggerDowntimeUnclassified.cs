7506b12b1c2988944578375e6ceca6a1e069ec27
shaneshka-xtensive <80815314+shaneshka-xtensive@users.noreply.github.com>
2021-08-18 11:24:11 +0500

    DPA-5773

--- DowntimeUnclassified/TriggerDowntimeUnclassified.cs
+++ DowntimeUnclassified/TriggerDowntimeUnclassified.cs
@@ -3,35 +3,36 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xtensive.Project109.Host.Base;
 namespace Xtensive.Project109.Host.DPA
 {
-	public class ZFTriggerDowntimeUnclassified : Signals2TriggerBase
+	public class TriggerDowntimeUnclassified : Signals2TriggerBase
 	{
-		private TimeSpan workerDelay = TimeSpan.FromMinutes(1);
-		private TimeSpan unclassifiedIgnoreDuration = TimeSpan.FromMinutes(7);
-		private TimeSpan[] unclassifiedLevelDuration = new[] { TimeSpan.FromMinutes(60), TimeSpan.FromMinutes(240) };
-		private readonly IHostLog<ZFTriggerDowntimeUnclassified> logger;
+		private SettingsDowntimeUnclassified settings;
+		private readonly IHostLog<TriggerDowntimeUnclassified> logger;
 		private readonly IDateTimeOffsetProvider dateTimeOffsetProvider;
 		private readonly IDowntimeReasonService downtimeReasonService;
-		private ConcurrentDictionary<long, ConcurrentDictionary<long, ZFTriggerDowntimeUnclassifiedItem>> equipmentUnclassifiedReasons;
+		private ConcurrentDictionary<long, ConcurrentDictionary<long, TriggerDowntimeUnclassifiedItem>> equipmentUnclassifiedReasons;
 		private IDisposable subscription;
 		private CancellationTokenSource cts;
 		private Task watcherTask;
 
-		public ZFTriggerDowntimeUnclassified(IServiceProvider serviceProvider)
+		public TriggerDowntimeUnclassified(IServiceProvider serviceProvider)
 		{
 			downtimeReasonService = serviceProvider.GetRequiredService<IDowntimeReasonService>();
-			logger = serviceProvider.GetRequiredService<IHostLog<ZFTriggerDowntimeUnclassified>>();
+			logger = serviceProvider.GetRequiredService<IHostLog<TriggerDowntimeUnclassified>>();
 			dateTimeOffsetProvider = serviceProvider.GetRequiredService<IDateTimeOffsetProvider>();
+			settings = new SettingsDowntimeUnclassified();
+			settings.EquipmentsSettings = settings.EquipmentsSettings.ToDictionary(x => x.Key, x => x.Value.OrderBy(y => y.Duration).ToList());
 		}
 		public override Task StartAsync()
 		{
 			cts = new CancellationTokenSource();
 			equipmentUnclassifiedReasons = new ConcurrentDictionary<long,
-			ConcurrentDictionary<long, ZFTriggerDowntimeUnclassifiedItem>>();
+			ConcurrentDictionary<long, TriggerDowntimeUnclassifiedItem>>();
 			subscription = downtimeReasonService.Subscribe(HandleReasonEvent);
 			watcherTask = Task.Run(() => Worker(cts.Token), cts.Token);
 			return Task.CompletedTask;
@@ -39,25 +40,30 @@ namespace Xtensive.Project109.Host.DPA
 		private void HandleReasonEvent(DowntimeReasonActionDto action)
 		{
 			logger.Info(string.Format("DowntimeReason {0} {1} {2}", action.Id, action.StartDate, action.EndDate));
-			ConcurrentDictionary<long, ZFTriggerDowntimeUnclassifiedItem> reasons;
+			if (!settings.EquipmentsSettings.ContainsKey(action.EquipmentId)) {
+				logger.Info(string.Format("skip equipmentId [{0}]", action.EquipmentId));
+				return;
+			}
+
+			ConcurrentDictionary<long, TriggerDowntimeUnclassifiedItem> reasons;
 			if (!equipmentUnclassifiedReasons.TryGetValue(action.EquipmentId, out reasons)) {
-				reasons = new ConcurrentDictionary<long, ZFTriggerDowntimeUnclassifiedItem>();
+				reasons = new ConcurrentDictionary<long, TriggerDowntimeUnclassifiedItem>();
 				equipmentUnclassifiedReasons.TryAdd(action.EquipmentId, reasons);
 			}
 			if (action.Status == DowntimeStatus.Ignored) {
 				if (action is DowntimeReasonCreatedActionDto) {
-					reasons.TryAdd(action.Id, new ZFTriggerDowntimeUnclassifiedItem(action));
+					reasons.TryAdd(action.Id, new TriggerDowntimeUnclassifiedItem(action));
 				}
 				else if (action is DowntimeReasonChangedActionDto) {
-					reasons.AddOrUpdate(action.Id, new ZFTriggerDowntimeUnclassifiedItem(action), (rId, a) => { a.Reason = action; return a; });
+					reasons.AddOrUpdate(action.Id, new TriggerDowntimeUnclassifiedItem(action), (rId, a) => { a.Reason = action; return a; });
 				}
 				else if (action is DowntimeReasonRemovedActionDto) {
-					ZFTriggerDowntimeUnclassifiedItem dto;
+					TriggerDowntimeUnclassifiedItem dto;
 					reasons.TryRemove(action.Id, out dto);
 				}
 			}
 			else {
-				ZFTriggerDowntimeUnclassifiedItem dto;
+				TriggerDowntimeUnclassifiedItem dto;
 				reasons.TryRemove(action.Id, out dto);
 			}
 		}
@@ -69,39 +75,42 @@ namespace Xtensive.Project109.Host.DPA
 					var now = dateTimeOffsetProvider.Now;
 					foreach (var equipmentReasons in equipmentUnclassifiedReasons) {
 						var equipmentId = equipmentReasons.Key;
-						var deleteIds = new List<long>();
-						try {
-							foreach (var reason in equipmentReasons.Value) {
-								token.ThrowIfCancellationRequested();
-								var t = now - reason.Value.Reason.StartDate;//EndDate
-								var end = reason.Value.Reason.EndDate.HasValue ? reason.Value.Reason.EndDate.Value : now;
-								var isIgnore = (end - reason.Value.Reason.StartDate) < unclassifiedIgnoreDuration;
+						List<EquipmentSettingsDowntimeUnclassified> equipmentSettings;
+						if (settings.EquipmentsSettings.TryGetValue(equipmentId, out equipmentSettings)) {
+							var deleteIds = new List<long>();
+							try {
+								foreach (var reason in equipmentReasons.Value) {
+									token.ThrowIfCancellationRequested();
+									var t = now - reason.Value.Reason.StartDate;//EndDate
+									var end = reason.Value.Reason.EndDate.HasValue ? reason.Value.Reason.EndDate.Value : now;
+									var isIgnore = (end - reason.Value.Reason.StartDate) < settings.UnclassifiedIgnoreDuration;
 
-								for (var i = unclassifiedLevelDuration.Length-1; i >=0; i--) {
-									if (t > unclassifiedLevelDuration[i]) {
-										logger.Info(equipmentId);
-										if (reason.Value.LastLevelIdSend == i - 1 && !isIgnore) {
-											OnSignal(new ZFDowntimeUnclassified {
-												EquipmentId = equipmentId,
-												StartDate=reason.Value.Reason.StartDate,
-												ReasonId = reason.Value.Reason.Id,
-												LevelId = i
-											});
-											reason.Value.LastLevelIdSend = i;
+									for (var i = equipmentSettings.Count - 1; i >= 0; i--) {
+										if (t > equipmentSettings[i].Duration) {
+											logger.Info(equipmentId);
+											if (reason.Value.LastLevelIdSend == i - 1 && !isIgnore) {
+												OnSignal(new CommonDowntimeUnclassified {
+													EquipmentId = equipmentId,
+													StartDate = reason.Value.Reason.StartDate,
+													ReasonId = reason.Value.Reason.Id,
+													LevelId = i
+												});
+												reason.Value.LastLevelIdSend = i;
+											}
+											if (i == equipmentSettings.Count - 1)
+												deleteIds.Add(reason.Key);
 										}
-										if(i == unclassifiedLevelDuration.Length - 1)
-											deleteIds.Add(reason.Key);
 									}
 								}
+								foreach (var rId in deleteIds) {
+									TriggerDowntimeUnclassifiedItem dto;
+									equipmentReasons.Value.TryRemove(rId, out dto);
+								}
 							}
-							foreach (var rId in deleteIds) {
-								ZFTriggerDowntimeUnclassifiedItem dto;
-								equipmentReasons.Value.TryRemove(rId, out dto);
+							catch (Exception e) {
+								logger.Error(e);
+								OnSignalError(e.Message);
 							}
-						}
-						catch (Exception e) {
-							logger.Error(e);
-							OnSignalError(e.Message);
 						}
 					}
 				}
@@ -109,7 +118,7 @@ namespace Xtensive.Project109.Host.DPA
 					logger.Error(ex);
 					OnSignalError(ex.Message);
 				}
-				await Task.Delay(workerDelay, token);
+				await Task.Delay(settings.WorkerDelay, token);
 			}
 		}
 		public override Task StopAsync()
@@ -124,11 +133,11 @@ namespace Xtensive.Project109.Host.DPA
 				cts.Dispose();
 			return Task.CompletedTask;
 		}
-		private class ZFTriggerDowntimeUnclassifiedItem
+		private class TriggerDowntimeUnclassifiedItem
 		{
 			public DowntimeReasonActionDto Reason { get; set; }
 			public int LastLevelIdSend { get; set; }
-			public ZFTriggerDowntimeUnclassifiedItem(DowntimeReasonActionDto reason, int lastLevelIdSend = -1)
+			public TriggerDowntimeUnclassifiedItem(DowntimeReasonActionDto reason, int lastLevelIdSend = -1)
 			{
 				Reason = reason;
 				LastLevelIdSend = lastLevelIdSend;
