780d75e43e2a9860122ef0e2b34366bcc563e76a
shaneshka-xtensive <80815314+shaneshka-xtensive@users.noreply.github.com>
2021-08-18 11:23:04 +0500

    DPA-5773

--- DowntimeUnclassified/CommonScriptDowntimeUnclassified.cs
+++ DowntimeUnclassified/CommonScriptDowntimeUnclassified.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
 
 namespace Xtensive.Project109.Host.DPA
 {
-	public class ZFDowntimeUnclassified
-	{ 
+	public class SettingsDowntimeUnclassified {
+		public TimeSpan WorkerDelay = TimeSpan.FromMinutes(1);
+		public TimeSpan UnclassifiedIgnoreDuration = TimeSpan.FromMinutes(7);
+		public Dictionary<long, List<EquipmentSettingsDowntimeUnclassified>> EquipmentsSettings
+		= new Dictionary<long, List<EquipmentSettingsDowntimeUnclassified>> {
+			//номер рабочего центра
+			{1, 
+				//настройки для этого центра
+				new List<EquipmentSettingsDowntimeUnclassified>{
+					new EquipmentSettingsDowntimeUnclassified {
+						Duration = TimeSpan.FromMinutes(60),
+						PersonnelNumbers = new string[] { "2015984033", "234" },
+						TemplateId = 3639519
+					},
+				}
+			}
+		};
+	}
+	public class EquipmentSettingsDowntimeUnclassified
+	{
+		/// <summary>
+		/// Через сколько послать
+		/// </summary>
+		public TimeSpan Duration { get; set; }
+		/// <summary>
+		/// Номер шаблона сообщения
+		/// </summary>
+		public long TemplateId { get; set; }
+		
+		/// <summary>
+		/// Группа получателей
+		/// </summary>
+		public long? GroupId { get; set; }
+		/// <summary>
+		/// Табельные номера
+		/// </summary>
+		public string[] PersonnelNumbers { get; set; }
+	}
+
+	public class CommonDowntimeUnclassified
+	{
 		public long EquipmentId { get; set; }
 		public DateTimeOffset StartDate { get; set; }
 		public long ReasonId { get; set; }
