5f228b8fea011a54969832e15a3dfa0d4f608a30
shaneshka-xtensive <80815314+shaneshka-xtensive@users.noreply.github.com>
2021-08-18 11:23:32 +0500

    DPA-5773

--- DowntimeUnclassified/HandlerDowntimeUnclassified.cs
+++ DowntimeUnclassified/HandlerDowntimeUnclassified.cs
@@ -8,65 +8,66 @@ using Xtensive.Orm;
 using Xtensive.Project109.Host.Base;
 namespace Xtensive.Project109.Host.DPA
 {
-	public class ZFHandlerDowntimeUnclassified : Signals2HandlerBase
+	public class HandlerDowntimeUnclassified : Signals2HandlerBase
 	{
-		private ZFHandlerDowntimeUnclassifiedLevelSetting[] levelSettings = new[] { 
-			//0 level
-			new ZFHandlerDowntimeUnclassifiedLevelSetting {
-				PersonnelNumbers = new string[] { "2015984033", "234" },
-				TemplateId = 3639519
-			},
-			//1 level
-			 new ZFHandlerDowntimeUnclassifiedLevelSetting {
-				PersonnelNumbers = new string[] { "123", "234" },
-				TemplateId = 3639519
-			}
-		};
+		private SettingsDowntimeUnclassified settings;
 		private readonly NotificationMessageTaskBuilder notificationMessageTaskBuilder;
-		private readonly IHostLog<ZFHandlerDowntimeUnclassified> logger;
-		public ZFHandlerDowntimeUnclassified(IServiceProvider serviceProvider)
+		private readonly IHostLog<HandlerDowntimeUnclassified> logger;
+		public HandlerDowntimeUnclassified(IServiceProvider serviceProvider)
 		{
 			notificationMessageTaskBuilder = serviceProvider.GetRequiredService<NotificationMessageTaskBuilder>();
-			logger = serviceProvider.GetRequiredService<IHostLog<ZFHandlerDowntimeUnclassified>>();
+			logger = serviceProvider.GetRequiredService<IHostLog<HandlerDowntimeUnclassified>>();
+			settings = new SettingsDowntimeUnclassified();
+			settings.EquipmentsSettings = settings.EquipmentsSettings.ToDictionary(x => x.Key, x => x.Value.OrderBy(y => y.Duration).ToList());
 		}
 		public override Task SignalHandleAsync(Signals2ScriptEventArgs args)
 		{
 			logger.Info(args);
-			if (args.Obj is ZFDowntimeUnclassified) {
-				var obj = (ZFDowntimeUnclassified)args.Obj;
+			if (args.Obj is CommonDowntimeUnclassified) {
+				var obj = (CommonDowntimeUnclassified)args.Obj;
 
 				var reason = Query.SingleOrDefault<DowntimeReason>(obj.ReasonId);
-				if (reason != null){
-					var record =reason.DowntimeInfo.Record;
+				if (reason != null) {
+					var record = reason.DowntimeInfo.Record;
 					if (record != null && record.Type == Xtensive.DPA.Contracts.MachineStateType.SwitchedOn) {
-						var settings = levelSettings[obj.LevelId];// maybe exception
-						var users = Query.All<DpaUser>().Where(x => x.PersonnelNumber.In(settings.PersonnelNumbers));
-						var equipmentName = Query.Single<Equipment>(obj.EquipmentId).Name;
-						var template = Query.All<MessageTemplate>().Single(t => t.Id == settings.TemplateId);
+						List<EquipmentSettingsDowntimeUnclassified> equipmentSettings;
+						if (settings.EquipmentsSettings.TryGetValue(obj.EquipmentId, out equipmentSettings)) {
+							var levelSettings = equipmentSettings[obj.LevelId];
+							var personnelNumbers = new List<string>();
+							if (levelSettings.PersonnelNumbers != null && levelSettings.PersonnelNumbers.Any())
+								personnelNumbers.AddRange(levelSettings.PersonnelNumbers);
+							if (levelSettings.GroupId.HasValue) {
+								var group = Query.Single<Xtensive.Project109.Host.Security.Group>(levelSettings.GroupId);
+								var pn = group.Childs.OfType<DpaUser>().Select(c => c.PersonnelNumber).ToArray();
+								if (pn != null && pn.Any())
+									personnelNumbers.AddRange(pn);
+							}
 
-						notificationMessageTaskBuilder.BuildAndScheduleMessages(
-						MessageTransportType.Email,
-						template,
-						users,
-						() => new Dictionary<string, string> { { "EquipmentName", equipmentName }, { "DonwtimeReasonForSignal.EventStartTime", obj.StartDate.ToString() } },
-						"Signals 2.0"
-						);
+							var users = Query.All<DpaUser>().Where(x => x.PersonnelNumber.In(personnelNumbers));
+							var equipmentName = Query.Single<Equipment>(obj.EquipmentId).Name;
+							var template = Query.All<MessageTemplate>().Single(t => t.Id == levelSettings.TemplateId);
+
+							notificationMessageTaskBuilder.BuildAndScheduleMessages(
+								MessageTransportType.Email,
+								template,
+								users.Distinct(),
+								() => new Dictionary<string, string> { { "EquipmentName", equipmentName }, { "DonwtimeReasonForSignal.EventStartTime", obj.StartDate.ToString() } },
+								"Signals 2.0"
+							);
+						}
+						else {
+							logger.Info(string.Format("not found settings for equipmentId [{0}]", obj.EquipmentId));
+						}
 					}
 					else {
-						logger.Info(string.Format("skip MachineStateRecordId [{0}]", obj.ReasonId));
+						logger.Info(string.Format("skip DowntimeReasonId [{0}],  record type [{1}]", obj.ReasonId, record.Type));
 					}
 				}
 				else {
-					logger.Info(string.Format("skip MachineStateRecordId [{0}]", obj.ReasonId));
+					logger.Info(string.Format("not found DowntimeReasonId [{0}]", obj.ReasonId));
 				}
 			}
 			return Task.CompletedTask;
-		}
-
-		class ZFHandlerDowntimeUnclassifiedLevelSetting
-		{
-			public string[] PersonnelNumbers { get; set; }
-			public long TemplateId { get; set; }
 		}
 	}
 }
