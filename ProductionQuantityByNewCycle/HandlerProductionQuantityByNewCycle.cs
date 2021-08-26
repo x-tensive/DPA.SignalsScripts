8a1e4128bde49a924cee8236e16ab90a6a75abce
shaneshka-xtensive <80815314+shaneshka-xtensive@users.noreply.github.com>
2021-08-26 15:20:45 +0500

    Create HandlerProductionQuantityByNewCycle.cs

---
+++ ProductionQuantityByNewCycle/HandlerProductionQuantityByNewCycle.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using Xtensive.Project109.Host.Base;
+
+namespace Xtensive.Project109.Host.DPA
+{
+	public class HandlerProductionQuantityByNewCycle : Signals2HandlerBase
+	{
+		private readonly IJobService jobService;
+		private readonly IHostLog<HandlerProductionQuantityByNewCycle> logger;
+
+		public HandlerProductionQuantityByNewCycle(IServiceProvider serviceProvider)
+		{
+			//equipmentService = serviceProvider.GetRequiredService<IEquipmentService>();
+			jobService = serviceProvider.GetRequiredService<IJobService>();
+			logger = serviceProvider.GetRequiredService<IHostLog<HandlerProductionQuantityByNewCycle>>();
+		}
+
+		public override Task SignalHandleAsync(Signals2ScriptEventArgs args)
+		{
+			logger.Info(args);
+
+			if (args.Obj is ProductionQuantityByNewCycle) {
+				var productionQuantity = (ProductionQuantityByNewCycle)args.Obj;
+				var job = jobService.GetActiveProduction(productionQuantity.EquipmentId);
+				if (job != null) {
+					jobService.AppendQuantity(job.Id, productionQuantity.QuantityModel, Array.Empty<OperatorComponentConsumptionDto>(), "signals2", null, DateTimeOffset.UtcNow, null);
+				}
+			}
+			return Task.CompletedTask;
+		}
+	}
+}
