103ca147f721179aa160a48ebfe40fb229f778e6
shaneshka-xtensive <80815314+shaneshka-xtensive@users.noreply.github.com>
2021-09-28 08:51:59 +0500

    Update CommonScriptProductionQuantityByNewCycle.cs

--- ProductionQuantityByNewCycle/CommonScriptProductionQuantityByNewCycle.cs
+++ ProductionQuantityByNewCycle/CommonScriptProductionQuantityByNewCycle.cs
@@ -6,11 +6,12 @@ namespace Xtensive.Project109.Host.DPA
 	public class ProductionQuantityByNewCycle 
 	{ 
 		public long EquipmentId { get; set; }
+		public long JobId { get; set; }
 		public QuantityModel QuantityModel { get; set; }
 
 		public override string ToString()
 		{
-			return string.Format("{0} {1} {2} {3}", EquipmentId, QuantityModel.Accepted, QuantityModel.Rejected, QuantityModel.Undefined);
+			return string.Format("{0} {1} {2} {3} {4}", EquipmentId, JobId, QuantityModel.Accepted, QuantityModel.Rejected, QuantityModel.Undefined);
 		}
 	}
 }
