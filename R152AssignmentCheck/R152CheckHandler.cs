6d67e2b27b5bdc86fda8f9813a000415bf523dbd
LyohaLyoha <alexey.lobov@x-tensive.com>
2022-07-13 20:03:28 +0500

    DPA-6249: Added filter by equipment groups before processing event by script, added source equipment name to message bodies

--- R152AssignmentCheck/R152CheckHandler.cs
+++ R152AssignmentCheck/R152CheckHandler.cs
@@ -28,15 +28,20 @@ namespace Xtensive.Project109.Host.DPA
 	{
 		/// <summary>
 		// "R152 NOT FOUND" message template must be added in Messenger microservice, with following message body:
-		// "R152 reference was not found in CP {ProgramName} body, production output accounting will not work"
+		// "R152 reference was not found in CP {ProgramName} body, production output accounting will not work on {EquipmentName} equipment"
 		/// </summary>
 		private const string NotFoundTemplateName = "R152 NOT FOUND";
 
 		/// <summary>
 		// "R152 NOT INCREASED" message template must be added in Messenger microservice, with following message body:
-		// "R152 reference exists in CP {ProgramName} body, but its value is never modified, production output accounting will not work"
+		// "R152 reference exists in CP {ProgramName} body, but its value is never modified, production output accounting will not work on {EquipmentName} equipment"
 		/// </summary>
 		private const string NotIncreasedTemplateName = "R152 NOT INCREASED";
+
+		/// <summary>
+		// Program download events only from equipment added to one of equipment groups with names from this list will be processed
+		/// </summary>
+		private string[] AffectedEquipmentGroupNames = new[] { "R152 GROUP" };
 
 		private readonly IJobService jobService;
 		private readonly NotificationMessageTaskBuilder notificationMessageTaskBuilder;
@@ -56,6 +61,12 @@ namespace Xtensive.Project109.Host.DPA
 					logger.LogError("Signal Handler received argument without DownloadProgramRecordModel payload for CP source code check for R152 register increment");
 					return Task.CompletedTask;
 				}
+				var equipment = Query.All<Equipment>().Where(eq => eq.DriverIdentifier == model.DriverIdentifier).FirstOrDefault();
+				if (equipment == null)
+					return Task.CompletedTask;
+				var eqGroupNames = equipment.EquipmentGroups.Select(eqGrp => eqGrp.Name.ToUpperInvariant()).ToArray();
+				if (!eqGroupNames.Any(egn => AffectedEquipmentGroupNames.Contains(egn)))
+					return Task.CompletedTask;
 				if (model.ProgramData == null) {
 					logger.LogError("Signal Handler received DownloadProgramRecordModel without CP source code check for R152 register increment");
 					return Task.CompletedTask;
@@ -63,15 +74,15 @@ namespace Xtensive.Project109.Host.DPA
 				var resultCheck = CheckRegisterR152(model.ProgramData);
 				switch (resultCheck) {
 					case RegisterCheckResult.Assigned:
-						logger.LogInformation($"Program {model.ProgramName} passed check for R152 register increment");
+						logger.LogInformation($"Program {model.ProgramName} passed check for R152 register increment on {equipment.Name} equipment");
 						break;
 					case RegisterCheckResult.Referenced:
-						logger.LogWarning($"Program {model.ProgramName} references R152 register in source code, but doesn't increment it explicitly");
-						EmitNotification(NotIncreasedTemplateName, model.ProgramName);
+						logger.LogWarning($"Program {model.ProgramName} references R152 register in source code, but doesn't increment it explicitly on {equipment.Name} equipment");
+						EmitNotification(NotIncreasedTemplateName, model.ProgramName, equipment.Name);
 						break;
 					case RegisterCheckResult.NotFound:
-						logger.LogError($"Program {model.ProgramName} doesn't reference R152 register in source code");
-						EmitNotification(NotFoundTemplateName, model.ProgramName);
+						logger.LogError($"Program {model.ProgramName} doesn't reference R152 register in source code on {equipment.Name} equipment");
+						EmitNotification(NotFoundTemplateName, model.ProgramName, equipment.Name);
 						break;
 					default:
 						throw new Exception($"Unknown outcome of CP source code check for R152 register increment: {resultCheck}");
@@ -106,7 +117,7 @@ namespace Xtensive.Project109.Host.DPA
 			return result;
 		}
 
-		private void EmitNotification(string msgTemplateName, string programName)
+		private void EmitNotification(string msgTemplateName, string programName, string equipmentName)
 		{
 			var msgTemplate = Query.All<MessageTemplate>().SingleOrDefault(tpl => tpl.Name.ToUpper() == msgTemplateName);
 			if (msgTemplate == null) {
@@ -123,7 +134,7 @@ namespace Xtensive.Project109.Host.DPA
 				MessageTransportType.Web,
 				msgTemplate,
 				techUsers,
-				() => new Dictionary<string, string> { { "ProgramName", programName } },
+				() => new Dictionary<string, string> { { "ProgramName", programName }, { "EquipmentName", equipmentName } },
 				"R152 increase check script, Signals 2.0"
 			);
 		}
